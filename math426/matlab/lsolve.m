c9b37eef64ae5f90aaa5c8c8ddf96d8c4eef6290
David Maxwell <damaxwell@alaska.edu>
2020-11-20 17:03:56 -0900

    auto-publish via rake

--- math426/matlab/lsolve.m
+++ math426/matlab/lsolve.m
@@ -1,17 +1,17 @@
-# Lower-triangular matrix solve
-# 
-# Returns the solution x of Lx=b where L is square and lower-triangular.
-#
-# E.g.
-# 
-# L = [1 0 0; -2 1 0; 1 2 1];
-# b = [1 ; -1; 2 ];
-#
-# x = lsolve(L,b)  -> x = [1; 1; -1 ]
+% // # Lower-triangular matrix solve
+% // # 
+% // # Returns the solution x of Lx=b where L is square and lower-triangular.
+% // #
+% // # E.g.
+% // # 
+% // # L = [1 0 0; -2 1 0; 1 2 1];
+% // # b = [1 ; -1; 2 ];
+% // #
+% // # x = lsolve(L,b)  -> x = [1; 1; -1 ]
 
 function x=lsolve(L,b)
-	n=rows(L);
-	m=columns(L);
+	n=size(L,1);
+	m=size(L,2);
 	if n~=m
 		error("matrix is not square")
 	end
